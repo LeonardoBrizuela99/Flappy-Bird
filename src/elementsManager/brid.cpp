aeb64faa504b398b1326147cc30b6d15d9ba96c8
Prieto Ezequiel <72413070+zequi-pv@users.noreply.github.com>
2023-11-16 21:11:03 -0300

    Change size bird

--- src/elementsManager/brid.cpp
+++ src/elementsManager/brid.cpp
@@ -3,7 +3,7 @@
 void initBird(Bird& bird)
 {
 	bird.pos = {static_cast<float>(GetScreenWidth() / 2 - 300), static_cast<float>(GetScreenHeight() / 2) };
-	bird.size = { 70.0f, 70.0f };
+	bird.size = { static_cast<float>(bird.textureOne.width), static_cast<float>(bird.textureOne.height) };
 	bird.speed = 150.0f;
 	bird.isRaising = false;
 }
